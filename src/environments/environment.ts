a2ae2e977141665954685be824afc7c87cb14c9d
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2018-03-10 11:16:52 +0100

    Add Firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBl66LhUZQYBCPtdmLq1snqDVSkqxk7kxI",
+    authDomain: "firechat-4d053.firebaseapp.com",
+    databaseURL: "https://firechat-4d053.firebaseio.com",
+    projectId: "firechat-4d053",
+    storageBucket: "firechat-4d053.appspot.com",
+    messagingSenderId: "802089573322"
+  }
 };
